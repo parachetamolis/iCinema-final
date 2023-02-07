e4a701a3d83f5be0b84a87adf6c05edb69525bfa
parachetamolis <eligijuss@outlook.com>
2023-02-07 21:39:09 +0200

    Refactored movie.js

--- controller/movie.js
+++ controller/movie.js
@@ -27,34 +27,40 @@ const upload = multer({ storage: storage }).single("image");
 
 exports.addMovie = (req, res) => {
   upload(req, res, (err) => {
-    if (err) res.status(500).json(err);
-    else {
-      fs.readFile(req.file.path, function (err, data) {
-        if (err) throw err;
-        else {
-          const contentType = req.file.mimetype;
-          const newMovie = new Movie({
-            _id: mongoose.Types.ObjectId(),
-            title: req.body.title,
-            numberInStock: req.body.numberInStock,
-            genre: req.body.genre,
-            image: { data, contentType },
-            rate: 0,
-          });
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      saveMovie(req, res);
+    }
+  });
+};
 
-          //Saving new movie in db
-          newMovie.save((err, movie) => {
-            if (err) res.status(500).json({ error: err });
-            else {
-              res.status(201).json({
-                message: "A new movie added.",
-                movie: movie,
-              });
-            }
+const saveMovie = (req, res) => {
+  fs.readFile(req.file.path, function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      const contentType = req.file.mimetype;
+      const newMovie = new Movie({
+        _id: mongoose.Types.ObjectId(),
+        title: req.body.title,
+        numberInStock: req.body.numberInStock,
+        genre: req.body.genre,
+        image: { data, contentType },
+        rate: 0,
+      });
+
+      //Saving new movie in db
+      newMovie.save((err, movie) => {
+        if (err) {
+          res.status(500).json({ error: err });
+        } else {
+          res.status(201).json({
+            message: "A new movie added.",
+            movie: movie,
           });
         }
       });
     }
   });
 };
-
